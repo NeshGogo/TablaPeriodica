8edc7c4ca99171d56fd06593eb5a244b7a283f41
Rafael Aguero Baez <rafaelaguero10@hotmail.com>
2020-02-03 17:32:09 -0400

    cree los estilos para la tabla periodica, el controlador, el modelo y la vista donde se mostraran los datos.

---
+++ Models/Elemento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TablaPeriodica.Models
+{
+    public class Elemento
+    {
+    }
+}
